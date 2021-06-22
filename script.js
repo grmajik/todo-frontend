fcd86db1d3e68e82459ab2113efe5c055d64958b
grmajik <84933604+grmajik@users.noreply.github.com>
2021-06-22 23:33:39 +0300

    Add files via upload
    Added sortable functionality

--- script.js
+++ script.js
@@ -5,7 +5,44 @@ const task = document.getElementById('task-name');
 const answersWrapper = document.querySelector('.answers-wrapper');
 const counter = document.querySelector('.counter');
 const appControls = document.querySelector('.app-controls');
-const clearTasks = document.querySelector('.completed a');
+const clearTasks = document.querySelector('.completed');
+
+// Listen in for drag events globally
+
+const container = document.querySelector('.answers-wrapper');
+
+document.addEventListener('dragstart', e => {
+	e.target.classList.add('dragging');
+})
+
+document.addEventListener('dragend', e => {
+	e.target.classList.remove('dragging');
+})
+
+container.addEventListener('dragover', (e) => {
+	e.preventDefault();
+	const afterElement = getDragAfterElement(container, e.clientY);
+	const draggable = document.querySelector('.dragging');
+	if (afterElement == null) {
+		container.appendChild(draggable);
+	} else {
+		container.insertBefore(draggable, afterElement);
+	}
+})
+
+function getDragAfterElement(container, y) {
+	const draggableElements = [...container.querySelectorAll('.answer-input:not(.dragging)')];
+	return draggableElements.reduce((closest, child) => {
+		const box = child.getBoundingClientRect();
+		const offset = y - box.top - box.height / 2;
+		if (offset < 0 && offset > closest.offset) {
+			return { offset: offset, element: child }
+		} else {
+			return closest;
+		}
+	}, { offset: Number.NEGATIVE_INFINITY }).element;
+}
+
 
 let divsCounter = 0;
 
@@ -35,16 +72,16 @@ add.addEventListener('click', (e) => {
 	e.preventDefault();
 	const newDiv = document.createElement('div');
 	newDiv.className = "answer-input";
+	newDiv.draggable = true;
 	const newCheckbox = document.createElement('input');
 	newCheckbox.type = "checkbox";
 	const newText = document.createElement('input');
 	newText.type = "text";
-	newText.className = "text";
+	newText.className = "text draggable";
 	newText.value = task.value;
 	const newButton = document.createElement('input');
 	newButton.type = 'button';
 	newButton.value = 'Remove';
-	newButton.id = "remove";
 	newDiv.appendChild(newCheckbox);
 	newDiv.appendChild(newText);
 	newDiv.appendChild(newButton);
